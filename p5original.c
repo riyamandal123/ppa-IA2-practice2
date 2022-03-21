716755d64f775645d1c1248c680c5ea4bc6a5b49
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-03-21 09:55:47 +0000

    p5original.c

--- p5original.c
+++ p5original.c
@@ -2,12 +2,12 @@
 
 int input()
 {
-  int num;
+  int n;
   printf("Enter a number:\n");
-  scanf("%d", &num);
-  return num;
+  scanf("%d", &n);
+  return n;
 }
-int gcd(int a, int b)
+int find_gcd(int a, int b)
 {
   int i,small,gcd;
   if ( a > b)
@@ -17,16 +17,19 @@ int gcd(int a, int b)
     b=small;
   }
   if( b%a==0)
+  {
     return a;
-
-  for(i=0;i<=a/2;i++)
+  }
+else
+{
+  for(i=1;i<=a/2;i++)
   {
     if ( a%i == 0 && b%i == 0)
       gcd = i;
   }
   return gcd;
-   
   }
+}
 void output(int a, int b, int gcd)
 {
   printf("The gcd of %d and %d is %d\n", a, b, gcd);
@@ -34,9 +37,10 @@ void output(int a, int b, int gcd)
 
 int main()
 {
-  int num1 = input();
-  int num2 = input();
-  int res = gcd(num1, num2);
-  output(num1, num2, res);
+  int a,b,gcd;
+  a = input();
+  b = input();
+  gcd = find_gcd(a,b);
+  output(a, b, gcd);
   return 0;
 }
