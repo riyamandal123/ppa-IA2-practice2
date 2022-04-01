2e92b54eaf2852d22d48b1be5352ffa4231b8c1c
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-04-01 03:17:57 +0000

    p3original.c

--- p3original.c
+++ p3original.c
@@ -8,19 +8,18 @@ int input_number()
 }
 int is_composite(int n,int composite)
 {
-   composite=0;
-  for(int i=1;i<=n;i++)
-  {
-    if(n%i==0)
-    {
-      composite +=1;
-    }
-  }
-  return composite;
+   for(int i=2;i<=n/2;i++)
+     {
+       if(n%i==0)
+       {
+         return 1;
+       }
+     }
+  return 0;
 }
 void output(int n,int result)
 {
-  if(result>2)
+  if(result==1)
   {
     printf("the number %d is composite\n",n);
   }
@@ -33,7 +32,7 @@ int main()
 {
   int n,composite,result;
   n=input_number();
-  is_composite(n,composite);
+  result=is_composite(n,composite);
   output(n,result);
   return 0;
 }
